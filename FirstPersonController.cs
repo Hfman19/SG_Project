3a0c4e1f98f6e01dbbd5137c5da866e68782f68b
Hfman19 <77929154+Hfman19@users.noreply.github.com>
2023-03-15 23:02:44 +0200

    Inertia parameter added
    I`ve added inertia parameter to player movements

--- FirstPersonController.cs
+++ FirstPersonController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -21,10 +21,12 @@ public class FirstPersonController : MonoBehaviour
     float rotationX = 0;
     private bool isCrouching = false;
     private float jumpCooldownTimer = 0f;
-private float runningCooldownTimer = 0f;
-private float runningDuration = 5f;
-private float runningRechargeDuration = 5f;
-private bool isRunningOnCooldown = false;
+    private float runningCooldownTimer = 0f;
+    private float runningDuration = 5f;
+    private float runningRechargeDuration = 5f;
+    private bool isRunningOnCooldown = false;
+    private float currentSpeed = 0f;
+    private Vector2 lastInput = Vector2.zero;
 
     [HideInInspector]
     public bool canMove = true;
@@ -41,90 +43,100 @@ private bool isRunningOnCooldown = false;
         originalHeight = characterController.height;
     }
 
-void Update()
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C) && canMove)
+        {
+            characterController.height /= 2f;
+            walkingSpeed /= 2f;
+            runningSpeed /= 2f;
+        }
+        else if (Input.GetKeyUp(KeyCode.C) && canMove)
+        {
+            characterController.height *= 2f;
+            walkingSpeed *= 2f;
+            runningSpeed *= 2f;
+        }
+
+        // Check if running is on cooldown
+        if (isRunningOnCooldown)
+        {
+            runningCooldownTimer -= Time.deltaTime;
+            if (runningCooldownTimer <= 0f)
+            {
+                isRunningOnCooldown = false;
+                runningDuration = 5f;
+                runningRechargeDuration = 5f;
+            }
+        }
+
+        // We are grounded, so recalculate move direction based on axes
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        // Press Left Shift to run
+        bool isRunning = Input.GetKey(KeyCode.LeftShift) && !isRunningOnCooldown;
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        // Add inertia to moves
+if (curSpeedX == 0 && moveDirection.magnitude > 0f)
 {
-    if (Input.GetKeyDown(KeyCode.C) && canMove)
-    {
-        characterController.height /= 2f;
-        walkingSpeed /= 2f;
-        runningSpeed /= 2f;
-    }
-    else if (Input.GetKeyUp(KeyCode.C) && canMove)
-    {
-        characterController.height *= 2f;
-        walkingSpeed *= 2f;
-        runningSpeed *= 2f;
-    }
+    moveDirection -= forward * Mathf.Sign(moveDirection.x) * Mathf.Min(moveDirection.magnitude, walkingSpeed) * Time.deltaTime;
+}
+if (curSpeedY == 0 && moveDirection.magnitude > 0f)
+{
+    moveDirection -= right * Mathf.Sign(moveDirection.z) * Mathf.Min(moveDirection.magnitude, walkingSpeed) * Time.deltaTime;
+}
 
-    // Check if running is on cooldown
-    if (isRunningOnCooldown)
-    {
-        runningCooldownTimer -= Time.deltaTime;
-        if (runningCooldownTimer <= 0f)
-        {
-            isRunningOnCooldown = false;
-            runningDuration = 5f;
-            runningRechargeDuration = 5f;
-        }
-    }
+float movementDirectionY = moveDirection.y;
+moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-    // We are grounded, so recalculate move direction based on axes
-    Vector3 forward = transform.TransformDirection(Vector3.forward);
-    Vector3 right = transform.TransformDirection(Vector3.right);
-    // Press Left Shift to run
-    bool isRunning = Input.GetKey(KeyCode.LeftShift) && !isRunningOnCooldown;
-    float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
-    float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
-    float movementDirectionY = moveDirection.y;
-    moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+// Apply jump cooldown
+if (jumpCooldownTimer > 0f)
+{
+    jumpCooldownTimer -= Time.deltaTime;
+}
 
-    // Apply jump cooldown
-    if (jumpCooldownTimer > 0f)
-    {
-        jumpCooldownTimer -= Time.deltaTime;
-    }
+if (Input.GetButton("Jump") && canMove && characterController.isGrounded && jumpCooldownTimer <= 0f)
+{
+    moveDirection.y = jumpSpeed;
+    jumpCooldownTimer = 1f;
+}
+else
+{
+    moveDirection.y = movementDirectionY;
+}
 
-    if (Input.GetButton("Jump") && canMove && characterController.isGrounded && jumpCooldownTimer <= 0f)
-    {
-        moveDirection.y = jumpSpeed;
-        jumpCooldownTimer = 1f;
-    }
-    else
-    {
-        moveDirection.y = movementDirectionY;
-    }
+// Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+// when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+// as an acceleration (ms^-2)
+if (!characterController.isGrounded)
+{
+    moveDirection.y -= gravity * Time.deltaTime;
+}
 
-    // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-    // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-    // as an acceleration (ms^-2)
-    if (!characterController.isGrounded)
-    {
-        moveDirection.y -= gravity * Time.deltaTime;
-    }
+// Move the controller
+characterController.Move(moveDirection * Time.deltaTime);
 
-    // Move the controller
-    characterController.Move(moveDirection * Time.deltaTime);
-
-    // Update running duration and recharge duration
-    if (isRunning)
+// Update running duration and recharge duration
+if (isRunning)
+{
+    runningDuration -= Time.deltaTime;
+    if (runningDuration <= 0f)
     {
-        runningDuration -= Time.deltaTime;
-        if (runningDuration <= 0f)
-        {
-            isRunningOnCooldown = true;
-            runningCooldownTimer = runningRechargeDuration;
-            runningDuration = 5f;
-            runningRechargeDuration += 5f; // increase recharge duration for next cooldown
-        }
-    }
-
-    // Player and Camera rotation
-    if (canMove)
-    {
-        rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        isRunningOnCooldown = true;
+        runningCooldownTimer = runningRechargeDuration;
+        runningDuration = 5f;
+        runningRechargeDuration += 5f; // increase recharge duration for next cooldown
     }
 }
+
+// Player and Camera rotation
+if (canMove)
+{
+    rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+    rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+    playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+    transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+}
+    }
 }
